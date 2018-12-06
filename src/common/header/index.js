19d2d55eac17e27b0111fb086eeadc32ca310e4e
wolter <mingcanchen@hotmail.com>
2018-12-07 00:15:28 +0800

    header 热搜

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,16 +1,50 @@
 import React from 'react';
 import {CSSTransition} from 'react-transition-group';
 import {connect} from 'react-redux';
+import { actionCreators } from './store';
 import {
 	HeaderWrapper,
 	Logo,
 	Nav,
 	NavItem,
 	SearchWrapper,
+	SearchInfo,
+	SearchInfoTitle,
+	SearchInfoSwitch,
+	SearchInfoItem,
+	SearchInfoList,
 	NavSearch,
 	Addition,
 	Button
 } from './style';
+
+// 定义热搜函数
+const getListArea= (show) => {
+	if (show) {
+		return (
+			<SearchInfo>
+				<SearchInfoTitle>
+					热门搜索
+					<SearchInfoSwitch>
+						换一批
+					</SearchInfoSwitch>
+				</SearchInfoTitle>
+				
+					<SearchInfoList>
+						<SearchInfoItem>教育</SearchInfoItem>
+						<SearchInfoItem>教育</SearchInfoItem>
+						<SearchInfoItem>教育</SearchInfoItem>
+						<SearchInfoItem>教育</SearchInfoItem>
+						<SearchInfoItem>教育</SearchInfoItem>
+						<SearchInfoItem>教育</SearchInfoItem>
+						<SearchInfoItem>教育</SearchInfoItem>
+					</SearchInfoList>
+			</SearchInfo>
+		)
+	}else {
+		return null;
+	}
+}
 
 const Header = (props) => {
 	return(
@@ -39,6 +73,9 @@ const Header = (props) => {
 					<i className={props.focused ? 'focused iconfont': 'iconfont'}>
 						&#xe60b;
 					</i>
+					
+					{getListArea(props.focused)}
+
 				</SearchWrapper>
 				<Addition>
 					<Button className='writting'>
@@ -54,7 +91,8 @@ const Header = (props) => {
 
 const mapStateToProps = (state) => {
 	return {
-		focused: state.focused 
+		focused: state.getIn(['header','focused'])
+		// focused: state.get('header').get('focused')
 	}
 }
 
@@ -63,19 +101,13 @@ const mapDispatchToProps =(dispatch) =>{
 	return {
 
 		handleInputFocus() {
-			const action = {
-				type: 'search_focus'
-			};
 
-			dispatch(action);
+			dispatch(actionCreators.searchFocus());
 		},
 
 		handleInputBlur() {
-			const action = {
-				type: 'search_blur'
-			};
 
-			dispatch(action);
+			dispatch(actionCreators.searchBlur());
 		}
 
 	}
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,8 @@
-import {createStore} from 'redux';
+import {createStore, compose, applyMiddleware} from 'redux';
 import reducer from './reducer';
-
-const store = createStore(reducer);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, /* preloadedState, */ composeEnhancers(
+    applyMiddleware()
+  ));
 
 export default store;
