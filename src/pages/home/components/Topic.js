b7706b93631fd63ce0a29ed3f17056e2d7a4b264
wolter <mingcanchen@hotmail.com>
2018-12-14 00:44:16 +0800

    首页topic优化

--- src/pages/home/components/Topic.js
+++ src/pages/home/components/Topic.js
@@ -5,21 +5,20 @@ import { connect } from 'react-redux';
 class Topic extends Component {
 
 	render() {
-
+		const {list} = this.props;
 		return (
 			<TopicWrapper>
 				{
-					this.props.list.map((item) => {
-						return (
-							<TopicItem key={item.get('id')}>
-								<img 
-									className='topic-pic'
-									src={item.get('imgUrl')}
-								/>
-								{item.get('title')}
-							</TopicItem>
-						);
-					})
+					list.map((item) => (						
+						<TopicItem key={item.get('id')}>
+							<img 
+								className='topic-pic'
+								src={item.get('imgUrl')}
+							/>
+
+							{item.get('title')}
+						</TopicItem>						
+					))
 				}
 
 			</TopicWrapper>
